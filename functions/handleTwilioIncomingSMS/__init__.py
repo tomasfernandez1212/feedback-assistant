2b4d5936ba2796f1a5fafef382b9a9a9d7055381
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-06-19 20:36:55 -0700

    Detects new phone numbers and adds to graph as well as the message.

--- functions/handleTwilioIncomingSMS/__init__.py
+++ functions/handleTwilioIncomingSMS/__init__.py
@@ -1,11 +1,14 @@
-import logging
 import azure.functions as func
+import logging
 
 from gremlin_python.driver import client, serializer, protocol
 from gremlin_python.driver.protocol import GremlinServerError
 import sys, os
 import traceback
 import asyncio
+
+from dotenv import load_dotenv
+load_dotenv()
 
 AZURE_COSMOS_HOST_NAME = os.environ.get("AZURE_COSMOS_HOST_NAME")
 AZURE_COSMOS_DB_NAME = os.environ.get("AZURE_COSMOS_DB_NAME")
@@ -21,10 +24,12 @@ client = client.Client(f'wss://{AZURE_COSMOS_HOST_NAME}.gremlin.cosmos.azure.com
                         message_serializer=serializer.GraphSONSerializersV2d0()
                         )
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Python HTTP trigger function processed a request.')
+
+    if AZURE_COSMOS_HOST_NAME is None:
+        raise Exception("AZURE_COSMOS_HOST_NAME is not set")
 
     # Unpack
     logging.info(req)
@@ -37,15 +42,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(f'Received message {body} from {from_number}')
 
     # Add Human to Graph
-    callback = client.submitAsync(f"g.V().hasLabel('human').has('phoneNumber', {from_number})")
-    if callback.result().one() is None:
-        # The vertex doesn't exist, add it
-        callback = client.submitAsync(f"g.addV('human').property('phoneNumber', {from_number})")
-        print("Vertex added")
+    callback = client.submitAsync(f"g.V().hasLabel('human').has('phoneNumber', '{from_number}')")
+    human_already_registered = callback.result().one() is None
+    logging.info(f"Human already registered: {human_already_registered}")
+    if human_already_registered:
+        logging.info(f"Human already in graph")
     else:
-        print("Vertex already exists")
+        callback = client.submitAsync(f"g.addV('human').property('phoneNumber', '{from_number}').property('pk', 'pk')")
+        logging.info(f"Added human to graph")
 
     # Add Message to Graph
-    callback = client.submitAsync(f"g.addV('message').property('text', {body}).property('type', 'human')")
+    callback = client.submitAsync(f"g.addV('message').property('text', '{body}').property('type', 'human').property('pk', 'pk')")
+    logging.info(f"Added message to graph")
 
     return func.HttpResponse("Received your message", status_code=200)
