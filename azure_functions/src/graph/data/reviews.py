198dc803629bea6ab861819f304ff147a4620374
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-07-25 13:18:54 -0700

    Added cleaning of review text.

--- azure_functions/src/graph/data/reviews.py
+++ azure_functions/src/graph/data/reviews.py
@@ -19,5 +19,10 @@ class Review(BaseModel):
     id: str = ""
 
     def model_post_init(self, __context: Any) -> None:
-        self.id: str = f"{self.source.name}_{self.source_review_id}"
+        self.id: str = f"{self.source.name}_{self.source_review_id}"  # Add ID
+        self.text: str = self._clean_text(self.text)  # Clean Text
         return super().model_post_init(__context)
+
+    def _clean_text(self, text: str) -> str:
+        text = text.replace("\n", "\\n").replace("'", "\\'")
+        return text
