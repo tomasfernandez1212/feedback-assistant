242cc0374d27ef8d3a5ce9a225aadd1922084472
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-07-26 18:57:02 -0700

    Added openai logic to get list of topic tags.

--- azure_functions/src/openai.py
+++ azure_functions/src/openai.py
@@ -39,3 +39,48 @@ class OpenAIInterface:
 
         score: int = json.loads(response["choices"][0]["message"]["function_call"]["arguments"])["score"]  # type: ignore
         return score  # type: ignore
+
+    def get_list_of_topics(self, text: str) -> list[str]:
+        response = openai.ChatCompletion.create(  # type: ignore
+            model="gpt-3.5-turbo-0613",
+            messages=[
+                {
+                    "role": "system",
+                    "content": """
+                    You are an expert in customer service. Your task is to identify the topics that are being discussed in customer's reviews, feedback, and in conversations with us. 
+                 
+                    For example, if a customer says "So glad to have tried the Souvla in the Marina. Parking is more reasonable, indoor and outdoor seating is plenty, and the food is just as good. I often get the black bean vegan “lamb” option in the gyro wrap and my wife gets their chicken salad. The mandarin oranges in the salad are SO good, you’ve gotta try it once. We also get the Greek fries, which are equally as delicious. And when you get to enjoy the meal with your pup in their sunny parklet, you won’t want to leave."
+                 
+                    You would identify the following topics: ["Reasonable Parking", "Plenty of Seating", "Good Food", "Black Bean Vegan 'Lamb'", "Chicken Salad", "Delicious Mandarin Oranges", "Delicious Greek Fries", "Sunny Parklet", "Dog Friendly"]
+                 
+                    The goal is to infer facts about the customer's experience.""",
+                },
+                {
+                    "role": "user",
+                    "content": f"What topics are being discussed here: {text}",
+                },
+            ],
+            functions=[
+                {
+                    "name": "report_topics",
+                    "description": "Used to report topics to the system.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "topics": {
+                                "type": "array",
+                                "description": "A list of topics.",
+                                "items": {"type": "string"},
+                            },
+                        },
+                    },
+                    "required": ["topics"],
+                }
+            ],
+            function_call={"name": "report_topics"},
+        )
+
+        logging.info("Response: %s", response)  # type: ignore
+
+        list_of_topics: list[str] = json.loads(response["choices"][0]["message"]["function_call"]["arguments"])["topics"]  # type: ignore
+        return list_of_topics  # type: ignore
