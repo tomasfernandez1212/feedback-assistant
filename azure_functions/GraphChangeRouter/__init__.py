3dd0d7fe1b567bcf151c371d70e89d1b1b1c1ca9
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-07-27 20:20:28 -0700

    Fixed graph change detector issues.

--- azure_functions/GraphChangeRouter/__init__.py
+++ azure_functions/GraphChangeRouter/__init__.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 import os
 import json
+import threading
 import requests
 
 from azure.functions import DocumentList
@@ -10,13 +11,17 @@ FUNCTION_APP_NAME = "feedback-assistant-function-app"
 
 
 def call_function(function_name: str, body: dict[str, str]):
+    # Get Function Key
     function_key = os.environ[f"{function_name}_KEY"]
     if function_key == "":
         raise Exception(f"{function_name}_KEY is not set")
+
+    # Create URL
     logging.info(f"Calling {function_name} with body {body}")
     url = f"https://{FUNCTION_APP_NAME}.azurewebsites.net/api/{function_name}?code={function_key}"
-    response = requests.post(url, json=body)
-    return response
+
+    # This starts the request, but doesn't wait for the response
+    threading.Thread(target=requests.post, args=(url,), kwargs={"json": body}).start()
 
 
 def main(nodes: DocumentList):
@@ -26,7 +31,9 @@ def main(nodes: DocumentList):
         id = node["id"]  # type: ignore
         label = node["label"]  # type: ignore
 
-        logging.info(f"Detected change to {id} of type {label} at {current_time}.")
+        logging.info(f"Detected change to {id} of type {label} at {current_time}")
 
-        handling_function_name = f"Handle{label}Change"  # type: ignore
-        call_function(handling_function_name, json.loads(node.to_json()))  # type: ignore
+        if label == "Review":
+            call_function("HandleReviewChange", json.loads(node.to_json()))  # type: ignore
+        if label == "FeedbackItem":
+            call_function("HandleFeedbackItemChange", json.loads(node.to_json()))  # type: ignore
