ee1d176ec2cace48c3a46f32c5ab22c323765ee8
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-08-01 17:33:21 -0700

    Improving clustering params

--- azure_functions/src/clustering.py
+++ azure_functions/src/clustering.py
@@ -4,7 +4,10 @@ from typing import List
 
 
 def cluster_embeddings(
-    embeddings: List[List[float]], min_cluster_size: int = 5
+    embeddings: List[List[float]],
+    min_cluster_size: int = 2,
+    min_samples: int = 5,
+    cluster_selection_method: str = "leaf",
 ) -> List[int]:
     """
     Cluster embeddings using HDBSCAN.
@@ -12,6 +15,11 @@ def cluster_embeddings(
     :param min_cluster_size: Minimum number of points in a cluster.
     :return: List of cluster labels, where each label is an integer.
     """
-    clusterer = HDBSCAN(min_cluster_size=min_cluster_size, gen_min_span_tree=True)
+    clusterer = HDBSCAN(
+        min_cluster_size=min_cluster_size,
+        min_samples=min_samples,
+        cluster_selection_method=cluster_selection_method,
+        gen_min_span_tree=True,
+    )
     cluster_labels = clusterer.fit_predict(embeddings)  # type: ignore
     return cluster_labels  # type: ignore
