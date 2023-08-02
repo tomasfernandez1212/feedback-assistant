5f9219abe884d44e3c6d98bcd456bfe96f2114ef
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-08-02 16:01:49 -0700

    REVIEW id

--- azure_functions/tests/handle_data_point_change_test.py
+++ azure_functions/tests/handle_data_point_change_test.py
@@ -19,19 +19,19 @@ class TestHandleDataPointChange(unittest.TestCase):
                 rating=Rating.FOUR,
                 date="2020-01-01T00:00:00Z",
                 source=ReviewSource.YELP,
-                source_review_id="REVIEW_485u0",
+                source_review_id="Review_485u0",
             )
             feedback_item = FeedbackItem(satisfaction_score=85, timestamp=345)
             storage.add_feedback_item(feedback_item, review)
             data_point_1 = DataPoint(
                 interpretation="Great Atmosphere",
                 embedding=f"{[random.random() for _ in range(1536)]}",
-                id="DATAPOINT_4048u",
+                id="DataPoint_4048u",
             )
             data_point_2 = DataPoint(
                 interpretation="Pastries",
                 embedding=f"{[random.random() for _ in range(1536)]}",
-                id="DATAPOINT_fh5894",
+                id="DataPoint_fh5894",
             )
             storage.add_data_point_for_feedback_item(data_point_1, feedback_item)
             storage.add_data_point_for_feedback_item(data_point_2, feedback_item)
