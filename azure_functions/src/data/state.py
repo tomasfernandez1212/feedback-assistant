4e4b72f3856f7aa81b3e6b2bddd0d9f21ad5d622
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-08-08 14:57:41 -0700

    Reset most of handle data point change function to avoid using timer based approach with state.

--- azure_functions/src/data/state.py
+++ azure_functions/src/data/state.py
@@ -2,6 +2,4 @@ from pydantic import BaseModel
 
 
 class AppState(BaseModel):
-    data_points_clustering_last_started: float = 0.0
-    data_points_last_modified: float = 0.0
     id: str = "only_app_state"
