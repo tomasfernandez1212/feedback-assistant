5275ed89bdfaf0d37c6b40326521f8b6e00c8a13
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-08-08 20:10:21 -0700

    Data point change function logic added and test modified.

--- azure_functions/HandleDataPointChange/__init__.py
+++ azure_functions/HandleDataPointChange/__init__.py
@@ -3,6 +3,7 @@ import logging
 import azure.functions as func
 from src.storage import Storage
 from src.data.dataPoint import DataPoint
+from src.openai import OpenAIInterface
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -14,5 +15,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.info(f"Getting DataPoint with ID: {id}")
         data_point = storage.get_node(id, DataPoint)
 
-        logging.info(f"Got DataPoint: {data_point}")
+        logging.info("Get related topics")
+        openai_interface = OpenAIInterface()
+        data_point_embedding = openai_interface.get_embedding(data_point.interpretation)
+        existing_topics = storage.search_with_embedding(DataPoint, data_point_embedding)
+        for existing_topic in existing_topics:
+            print(existing_topic)
+
         return func.HttpResponse("Done")
--- azure_functions/tests/handle_data_point_change_test.py
+++ azure_functions/tests/handle_data_point_change_test.py
@@ -7,6 +7,8 @@ from HandleDataPointChange import main
 from src.storage import Storage
 from src.data.dataPoint import DataPoint
 from src.data.feedbackItems import FeedbackItem
+from src.data.actionItems import ActionItem
+from src.data.topics import Topic
 from src.data.reviews import Review, Rating, ReviewSource
 from src.misc import iso_to_unix_timestamp
 
@@ -20,26 +22,36 @@ class TestHandleDataPointChange(unittest.TestCase):
                 source_review_id="Review_485u0",
             )
             feedback_item = FeedbackItem(
-                text="This place had a great atmosphere and the pastries were delicious!",
+                text="I just love how simple Souvla's menu is and you really can't go wrong with anything. My favorite dish is the chicken salad, it tastes so fresh but I wish it were bigger.",
                 text_written_at=iso_to_unix_timestamp("2023-07-25T00:00:00.000Z"),
             )
-            storage.add_feedback_item_and_source(feedback_item, review)
             data_point_1 = DataPoint(
-                interpretation="Great Atmosphere",
+                interpretation="The menu at Souvla is simple and all the dishes are highly recommended.",
                 id="DataPoint_4048u",
             )
             data_point_2 = DataPoint(
-                interpretation="Pastries",
+                interpretation="The customer finds the chicken salad fresh and satisfying.",
                 id="DataPoint_fh5894",
             )
+            topic_1 = Topic(
+                name="Chicken Salad",
+            )
+            action_item_1 = ActionItem(
+                text="Consider making the size of the chicken salad bigger.",
+            )
+            storage.add_feedback_item_and_source(feedback_item, review)
             storage.add_data_point_for_feedback_item(data_point_1, feedback_item)
             storage.add_data_point_for_feedback_item(data_point_2, feedback_item)
+            storage.add_topic(topic_1)
+            storage.add_action_item(action_item_1)
 
     def teardown_method(self, method: Callable[[], Any]):
         pass
 
     def test_handle_data_point_change(self):
-        req = mock.Mock(spec=func.TimerRequest)
-        req.past_due.return_value = False  # type: ignore
+        req = mock.Mock(spec=func.HttpRequest)
+        req.get_json.return_value = {"id": "DataPoint_4048u"}  # type: ignore
 
-        main(req)
+        resp = main(req)
+
+        assert resp.status_code == 200
