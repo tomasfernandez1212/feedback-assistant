a2d046a1b9a6ba71c32c1857375a69fb34a7d33d
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-08-16 22:46:32 -0700

    To json needed.

--- azure_functions/GraphChangeRouter/__init__.py
+++ azure_functions/GraphChangeRouter/__init__.py
@@ -14,7 +14,7 @@ def main(
     for node in documents:  # type: ignore
         node_id: str = node["id"]  # type: ignore
         node_label: str = node["label"]  # type: ignore
-        node_str: str = str(node)  # type: ignore
+        node_str: str = node.to_json()  # type: ignore
 
         logging.info(
             f"Detected change to {node_id} of type {node_label} at {current_time}"
@@ -22,9 +22,9 @@ def main(
         logging.info(f"Node: {node_str}")
 
         if node_label == "FeedbackItem":
-            feedbackitemchangequeue.set(node_str)
+            feedbackitemchangequeue.set(node_str)  # type: ignore
         elif node_label == "DataPoint":
-            datapointchangequeue.set(node_str)
+            datapointchangequeue.set(node_str)  # type: ignore
         else:
             logging.info(
                 f"Node {node_id} of type {node_label} does not have a queue to send to."
