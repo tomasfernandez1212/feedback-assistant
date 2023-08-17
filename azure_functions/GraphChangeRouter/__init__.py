edf19f7d002499cc74ad41f6faa1f3e18d8fe6af
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-08-16 20:47:44 -0700

    GraphCHangeRouter changes to use service bus.

--- azure_functions/GraphChangeRouter/__init__.py
+++ azure_functions/GraphChangeRouter/__init__.py
@@ -1,46 +1,52 @@
 import logging
 from datetime import datetime
+
 import os
-import json
-import threading
-import requests
+from typing import Dict, List
+
+from azure.servicebus.aio import ServiceBusClient, ServiceBusSender
+from azure.servicebus import ServiceBusMessage
 
 from azure.functions import DocumentList
 
-FUNCTION_APP_NAME = "fa-function-app"
+NAMESPACE_CONNECTION_STR = os.environ["MESSAGE_QUEUE_CONNECTION"]
+LABELS_WITH_QUEUE: List[str] = ["FeedbackItem", "DataPoint"]
+
+def label_to_queue_name(label: str) -> str:
+    return f"{label.lower()}changequeue"
+
+ALL_QUEUE_NAMES = [label_to_queue_name(label) for label in LABELS_WITH_QUEUE]
+
+def init_servicebus_senders() -> Dict[str, ServiceBusSender]:
+    
+    servicebus_client = ServiceBusClient.from_connection_string(conn_str=NAMESPACE_CONNECTION_STR, logging_enable=True) as servicebus_client:
+    
+    senders: Dict[str, ServiceBusSender] = {}
+    for queue_name in ALL_QUEUE_NAMES:
+        senders[queue_name] = servicebus_client.get_queue_sender(queue_name=queue_name)
+    return senders
 
 
-def call_function(function_name: str, body: dict[str, str]):
-    # Get Function Key
-    function_key = os.environ[f"{function_name}_KEY"]
-    if function_key == "":
-        raise Exception(f"{function_name}_KEY is not set")
 
-    # Create URL
-    logging.info(f"Calling {function_name}.")
-    url = f"https://{FUNCTION_APP_NAME}.azurewebsites.net/api/{function_name}?code={function_key}"
-
-    # This starts the request, but doesn't wait for the response
-    threading.Thread(target=requests.post, args=(url,), kwargs={"json": body}).start()
-
-
-def main(documents: DocumentList):
+def main(
+    documents: DocumentList,
+):
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logging.info(f"Python function started at {current_time}")
+
+    senders = init_servicebus_senders()
 
     for node in documents:  # type: ignore
-        id = node["id"]  # type: ignore
-        label = node["label"]  # type: ignore
+        node_id: str = node["id"]  # type: ignore
+        node_label: str = node["label"]  # type: ignore
 
-        logging.info(f"Detected change to {id} of type {label} at {current_time}")
+        logging.info(f"Detected change to {node_id} of type {node_label} at {current_time}")
         logging.info(f"Node: {node.to_json()}")  # type: ignore
 
-        if label == "FeedbackItem":
-            call_function("HandleFeedbackItemChange", json.loads(node.to_json()))  # type: ignore
-        elif label == "DataPoint":
-            call_function("HandleDataPointChange", json.loads(node.to_json()))  # type: ignore
-        elif label == "Topic":
-            call_function("HandleTopicChange", json.loads(node.to_json()))  # type: ignore
-        elif label == "ActionItem":
-            call_function("HandleActionItemChange", json.loads(node.to_json()))  # type: ignore
+        if node_label in LABELS_WITH_QUEUE:
+            queue_name = label_to_queue_name(node_label) # type: ignore
+            sender = senders[queue_name]
+            sender.send(ServiceBusMessage(node.to_json()))  # type: ignore
         else:
-            logging.info(f"Unknown node type: {label}")
+            logging.info(f"Node {node_id} of type {node_label} does not have a queue to send to.")
+            
