b36a216332ebe77cd7c6f7cf9f4b32d534bd76ad
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-08-18 18:49:34 -0700

    Got the router to work by limiting the cosmos batch to 1. The output binding for the service bus is intended to send 1 message at a time.

--- azure_functions/GraphChangeRouter/__init__.py
+++ azure_functions/GraphChangeRouter/__init__.py
@@ -5,17 +5,6 @@ from azure.functions import DocumentList, Out
 LABELS_WITH_QUEUES = ["FeedbackItem", "DataPoint", "ActionItem", "Topic"]
 
 
-def send_to_queue(node_id: str, node_label: str, node_str: str, queue: Out[str]):
-    info = f"send message to {node_label} queue for {node_id}"
-    logging.info(f"Attempting: {info}")
-    try:
-        queue.set(node_str)  # type: ignore
-        logging.info(f"Success: {info}")
-    except Exception as e:
-        logging.info(f"Failure: {info}")
-        logging.info(e)
-
-
 def main(
     documents: DocumentList,
     feedbackitemchangequeue: Out[str],
@@ -26,26 +15,32 @@ def main(
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logging.info(f"Python function started at {current_time}")
 
-    # Loop through documents with changes
-    for node in documents:  # type: ignore
-        node_id: str = node["id"]  # type: ignore
-        node_label: str = node["label"]  # type: ignore
-        node_str: str = node.to_json()  # type: ignore
+    if len(documents) > 1:
+        logging.error(
+            "More than one document was passed to the function. The output binding to the azure service bus queue can only send one message at a time. Set the maxItemsPerInvocation to 1 in the input binding."
+        )
+        return
 
+    node = documents[0]  # type: ignore
+
+    node_id: str = node["id"]  # type: ignore
+    node_label: str = node["label"]  # type: ignore
+    node_str: str = node.to_json()  # type: ignore
+
+    logging.info(f"Detected change to {node_id} of type {node_label} at {current_time}")
+
+    # Send node to appropriate queue
+    if node_label == "FeedbackItem":
+        feedbackitemchangequeue.set(node_str)  # type: ignore
+    elif node_label == "DataPoint":
+        datapointchangequeue.set(node_str)  # type: ignore
+    elif node_label == "ActionItem":
+        actionitemchangequeue.set(node_str)  # type: ignore
+    elif node_label == "Topic":
+        topicchangequeue.set(node_str)  # type: ignore
+    else:
         logging.info(
-            f"Detected change to {node_id} of type {node_label} at {current_time}"
+            f"Node {node_id} of type {node_label} does not have a queue to send to."
         )
 
-        # Send node to appropriate queue
-        if node_label == "FeedbackItem":
-            send_to_queue(node_id, node_label, node_str, feedbackitemchangequeue)  # type: ignore
-        elif node_label == "DataPoint":
-            send_to_queue(node_id, node_label, node_str, datapointchangequeue)  # type: ignore
-        elif node_label == "ActionItem":
-            send_to_queue(node_id, node_label, node_str, actionitemchangequeue)  # type: ignore
-        elif node_label == "Topic":
-            send_to_queue(node_id, node_label, node_str, topicchangequeue)  # type: ignore
-        else:
-            logging.info(
-                f"Node {node_id} of type {node_label} does not have a queue to send to."
-            )
+    logging.info(f"Python function finished.")
