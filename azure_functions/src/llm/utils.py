c59962f5ce80961ba7b0da8496c7cb5fc1e4e608
tomasfernandez1212 <tomasfernandez1212@gmail.com>
2023-08-21 21:40:17 -0700

    Made removal of trailing comma applicable more generally.

--- azure_functions/src/llm/utils.py
+++ azure_functions/src/llm/utils.py
@@ -1,23 +1,7 @@
 import openai
 import re
 import json
-import logging
 from typing import Dict, Any, List
-
-
-def is_trailing_comma_error(json_str: str, exception: Exception) -> bool:
-    """
-    Check if error in JSON parsing is because of trailing comma.
-    """
-    if "Expecting property name enclosed in double quotes" in str(exception):
-        last_open_brace = json_str.rfind("{")
-        last_close_brace = json_str.rfind("}")
-
-        if last_open_brace < last_close_brace:
-            snippet = json_str[last_open_brace : last_close_brace + 1]
-            if snippet.rstrip().endswith(",\n}"):
-                return True
-    return False
 
 
 def fix_trailing_commas(json_str: str) -> str:
@@ -35,13 +19,11 @@ def unpack_function_call_arguments(response: Any) -> Dict[str, Any]:
     arguments_str = response["choices"][0]["message"]["function_call"]["arguments"]
     try:
         arguments = json.loads(arguments_str)
-    except json.decoder.JSONDecodeError as e:
-        if is_trailing_comma_error(arguments_str, e):
-            arguments_str = fix_trailing_commas(arguments_str)
-            arguments = json.loads(arguments_str)
-        else:
-            logging.error(f"Error unpacking function call arguments: {arguments_str}")
-            raise e
+    except:
+        # Try to fix trailing commas
+        arguments_str = fix_trailing_commas(arguments_str)
+        arguments = json.loads(arguments_str)
+
     return arguments
 
 
